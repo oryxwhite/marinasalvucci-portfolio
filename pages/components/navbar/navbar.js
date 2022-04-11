e81464f37b8a4e1d24501c5348d5ed2bd0ef735a
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-04-11 18:48:19 -0400

    fix for navbar import

--- pages/components/navbar/navbar.js
+++ pages/components/navbar/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
   <nav onClick={() => setIsOpen(!isOpen)} className="relative container mx-auto">
-    <div   className="flex justify-end container mx-auto mt-14 px-10 -space-x-[3.2rem]">f
+    <div className="flex justify-end container mx-auto mt-14 px-10 -space-x-[3.2rem]">
         
         <motion.div animate={isOpen ? { rotate: 45} : {x: 0, rotate: 45}} transition={{duration: .2, ease: 'easeInOut'}} className='relative w-10 sm:w-16 h-[3px] bg-white rotate-45' />
         <motion.span animate={isOpen ? {rotate: -45} : {x: 0, rotate: 45}} transition={{duration: .2, ease: 'easeInOut'}} className={`w-10 sm:w-16 h-[3px] bg-white rotate-45 mx-12  `} />
@@ -19,11 +19,6 @@ export default function Navbar() {
         <motion.h2 animate={isOpen ? { opacity: 1} : { opacity: 0}} transition={{duration: .2, ease: 'easeInOut'}} className='text-white '>CONTACT</motion.h2>
         
      </div>   
-     </nav>   
-
-    
-    
-    
-    
-  )
-}
+   </nav>   
+   
+  )}
--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
        />
        
        <PortfolioItem name='Country Search' src='countsearchprev'
-        description={<div>A<strong> React </strong>app for sorting and searching through the info provided by the <a className=' text-red-400' href=' '>REST Countries API</a></div>}
+        description={<div>A<strong> React </strong>app for sorting and searching through the info provided by the <a className=' text-red-400' href=''>REST Countries API</a></div>}
         link='https://countrysearch-139e4.web.app/'
         alt='country search screenshot'
         />
