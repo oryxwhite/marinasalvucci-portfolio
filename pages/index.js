73b8ddfffe6d72d6204e7021c668717f77004b7f
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-04-11 18:41:29 -0400

    another description fix

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home() {
 
        <SlashDark />
        <PortfolioItem name='Lyra' source={'lyrasc'}
-        description={["An interactive audio collage built on community submitted content.  Built with the", <strong> MERN </strong>, "stack using ",<strong>Tone.JS </strong>,  "and ", <strong>Bootstrap</strong> ]}
+        description={<div> An interactive audio collage built on community submitted content.  Built with the <strong> MERN </strong>stack using <strong>Tone.JS </strong>and <strong>Bootstrap</strong> </div>}
        alt='lyra screenshot'
         
        />
        
        <PortfolioItem name='Country Search' src='countsearchprev'
-        description={<div>A<strong> React </strong>app for sorting and searching through the info provided by the <a className=' text-red-400' href=''>REST Countries API</a></div>}
+        description={<div>A<strong> React </strong>app for sorting and searching through the info provided by the <a className=' text-red-400' href=' '>REST Countries API</a></div>}
         link='https://countrysearch-139e4.web.app/'
         alt='country search screenshot'
         />
