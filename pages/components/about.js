1ba31cf09da7cb2a8fb1f2a694c33d6341fcb510
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-04-13 19:09:00 -0400

    added link to about section

--- pages/components/about.js
+++ pages/components/about.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function About() {
     return (
-        <Flex id="work">
+        <Flex id="about">
         <Box className="flex flex-col justify-center items-center" mt='12em'>
             <Box right="-25px" position='relative' bg='#565656' w='137px' h='5px' transform='rotate(42deg)' />
             <Box right='0px' position='relative' bg='#565656' w='137px' h='5px' transform='rotate(42deg)' />
@@ -14,7 +14,7 @@ export default function About() {
             <div className="text-serif w-11/12 mt-[1em] text-white text-center">
                <p> Hello! My name is <strong className="text-[#629add]">Marina</strong> </p>
                <p className="mt-4"> I love building elegant, responsive front ends with a focus on performance, SEO optimization, and accessibility.  </p>
-               <p className="mt-4"> I’m interested in user interactivity, devloping design systems, web audio, and other creative applications of web technology. </p>
+               <p className="mt-4"> I’m interested in user interactivity, devloping design systems, web audio, and creative applications of web technology. </p>
                <p className="mt-4"> My skills include API integration, E-commerce solutions including web store setup and payment processing, headless CMS integration, CSS animation, and mobile-first design. </p>
                <p className="mt-4"> In my free time I like to DJ at local clubs and make music with my modular synthesizer. </p>
             </div>
--- pages/components/navbar/navbar.js
+++ pages/components/navbar/navbar.js
@@ -9,12 +9,11 @@ export default function Navbar() {
         
         <motion.div animate={isOpen ? { rotate: 45} : {x: 0, rotate: 45}} transition={{duration: .2, ease: 'easeInOut'}} className='relative w-10  h-[3px] bg-white rotate-45' />
         <motion.span animate={isOpen ? {rotate: -45} : {x: 0, rotate: 45}} transition={{duration: .2, ease: 'easeInOut'}} className={`w-10  h-[3px] bg-white rotate-45 mx-12  `} />
-        
         <motion.span animate={isOpen ? { rotate: 45} : { rotate: 45}} transition={{duration: .2, ease: 'easeInOut'}} className="w-10  h-[3px] bg-white rotate-45 mx-12" /> 
         
      </div>
      <div className='flex flex-row justify-end mx-[94px] space-x-[2.4rem] -mt-[10px] font-serif'>
-        <motion.a animate={isOpen ? { opacity: 1} : { opacity: 0}} transition={{duration: .2, ease: 'easeInOut'}} className='text-white hover:text-[#2a69b6]'>ABOUT</motion.a>
+        <motion.a animate={isOpen ? { opacity: 1} : { opacity: 0}} transition={{duration: .2, ease: 'easeInOut'}} href='#about'className='text-white hover:text-[#2a69b6]'>ABOUT</motion.a>
         <motion.a animate={isOpen ? { opacity: 1} : { opacity: 0}} transition={{duration: .2, ease: 'easeInOut'}} href='#work' className='text-white hover:text-[#2a69b6] cursor-pointer'>WORK</motion.a>
         <motion.a animate={isOpen ? { opacity: 1} : { opacity: 0}} transition={{duration: .2, ease: 'easeInOut'}} href="#contact" className='text-white hover:text-[#2a69b6]'>CONTACT</motion.a>
         
