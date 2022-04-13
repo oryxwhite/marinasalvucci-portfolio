532c8b6a386e2ca334b05a51ef7e166e0957521a
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-04-13 18:14:54 -0400

    added tech icons

--- pages/components/about.js
+++ pages/components/about.js
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading } from "@chakra-ui/react"
+import Link from "next/link"
 
 export default function About() {
     return (
@@ -7,14 +8,31 @@ export default function About() {
             <Box right="-25px" position='relative' bg='#565656' w='137px' h='5px' transform='rotate(42deg)' />
             <Box right='0px' position='relative' bg='#565656' w='137px' h='5px' transform='rotate(42deg)' />
             <Box  right='25px'bg='#565656' position='relative' w='137px' h='5px' transform='rotate(42deg)' />
+
             <Heading fontWeight='600' fontSize='48px' bottom='40px' left='3px' position='relative' fontFamily='Playfair Display' color='white'> About </Heading>
-            <p className="text-serif w-11/12 mt-20 text-white text-center">
+
+            <p className="text-serif w-11/12 mt-[1em] text-white text-center">
                 Hello! My name is <strong className="text-[#629add]">Marina</strong> <br/> <br/>
                 I love building elegant, responsive front ends with a focus on performance, SEO optimization, and accessibility.  <br /> <br />
                 I’m interested in user interactivity, devloping design systems, web audio, and other creative applications of web technology. <br /> <br />
                 My skills include API integration, E-commerce solutions including web store setup and payment processing, headless CMS integration, CSS animation, and mobile-first design. <br /> <br />
-                In my free time I DJ at local clubs and make music with my modular synthesizer. 
+                In my free time I like to DJ at local clubs and make music with my modular synthesizer. 
             </p>
+
+            <div className=" grid-flow-row grid-cols-4 grid-rows-2 gap-y-8 gap-x-8 grid mt-10">
+                <img src='react.png' className="icon" />
+                <img src='nextjs.png' className="icon rounded-md" />    
+                <img src='node.png' className="icon" />    
+                <img src='tailwind.png' className="icon" />    
+                <img src='framer-motion.png' className="icon bg-white p-1 rounded-md" />    
+                <img src='mongo.png' className="icon bg-white p-[2px] rounded-md" />    
+                  
+                <img src='cmd.png' className="icon bg-white p-1 rounded-md" />    
+                <img src='wordpress.png' className="icon bg-white p-1 rounded-md" />    
+
+            </div>
+
+
         </Box>
         </Flex>
     )
--- pages/components/portfolio-item/portfolio-item.js
+++ pages/components/portfolio-item/portfolio-item.js
@@ -9,7 +9,7 @@ export default function PortfolioItem(props) {
   // const lyraDescription = <div> An interactive audio collage built on community submitted content.  Built with the <strong> MERN </strong>stack using <strong>Tone.JS </strong>and <strong>Bootstrap</strong> </div>
   // const countryDescription = <div>A<strong> React </strong>app for sorting and searching through the info provided by the <a className=' text-red-400'>REST Countries API</a></div>
   return (
-    <div className="flex flex-col items-center mt-[5em] bg-black w-[90vw] max-w-[550px] border-[3px] border-white " >
+    <div className="flex flex-col items-center mt-[2em] bg-black w-[90vw] max-w-[550px] border-[3px] border-white " >
       <div className="mt-[3em] w-[79%] block" >
         <a href={props.link} target='_blank' rel="noreferrer"><Image layout='responsive' alt={props.alt} src={props.source==="lyrasc" ? lyrasc : countsearchprev} /></a>
       </div>
